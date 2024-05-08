fcef4f66f71b9cb6fb6ca250afb72bbcbedffdb9
walopez <walter.lopez@microsoft.com>
2024-05-07 22:21:12 -0600

    Replaced date string by NOW and added test for CPU/memory used by the containers.

--- scanaks.sh
+++ scanaks.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 #################
 #This script will gather logs from an AKS cluster. The goal is to collect those logs for troubleshooting purposes.
-#This script is work in progress. It is developed in best effort.
+#This script is work in progress. It is developed in best effort. Lots of improvements are expected.
 #
 #Author: Walter Lopez <walter.lopez[at]microsoft.com>
 #
@@ -21,6 +21,8 @@ read -p "Enter the RESOURCE_GROUP: " RESOURCE_GROUP
 # Prompt the user to enter the CLUSTER_NAME
 read -p "Enter the CLUSTER_NAME: " CLUSTER_NAME
 
+NOW=`date +%F_%H-%M-%S_%z`
+
 #General information
 ####################
 
@@ -34,17 +36,20 @@ kubectl version --short > kubectl-version.txt
 kubectl cluster-info > k_cluster-info.txt
 
 #Export details about the AKS cluster.
-az aks show -g $RESOURCE_GROUP -n $CLUSTER_NAME > az-aks-show_`date +%F_%H-%M-%S_%z`.json
+az aks show -g $RESOURCE_GROUP -n $CLUSTER_NAME > az-aks-show_$NOW.json
 
 #Check out the existing worker nodes of the AKS cluster.
-kubectl get nodes -o wide > k_nodes_`date +%F_%H-%M-%S_%z`.txt
+kubectl get nodes -o wide > k_nodes_$NOW.txt
 
 #Check out the existing pods of the AKS cluster.
-kubectl get pods -A -o wide > k_pods_`date +%F_%H-%M-%S_%z`.txt
+kubectl get pods -A -o wide > k_pods_$NOW.txt
 
 #Create a dump of the events in the cluster.
-kubectl get events > k_get-events_`date +%F_%H-%M-%S_%z`.txt
+kubectl get events > k_get-events_$NOW.txt
 
-#Check the CPU/memory of the nodes
-kubectl top nodes
+#Check the CPU/memory of the nodes.
+kubectl top nodes > k_top_nodes_$NOW.txt
+
+#Check the CPU/memory used by the containers.
+kubectl top pods -A > k_top_pods_$NOW.txt
 
